1c77dc3a2fba3fe912c40aacd7f574a41e3144e8
Gunter Sachse <gsachse@gmx.net>
2018-03-16 07:43:23 +0100

    initial commit

---
+++ src/components/myreservations/overview/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    Image,
+    ScrollView,
+    ListView,
+    TouchableOpacity,
+} from 'react-native'
+import styles from './styles';
+
+export default class MyReservationsOverview extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            station: false,
+            bike: false,
+            switchCharge: false,
+        };
+    }
+
+    componentDidMount() {
+        // check if props else get data again?
+        if (this.props.hasOwnProperty('station')) {
+            this.setState({station: this.props.station});
+        } else {
+            this.setState({station: mockStation});
+        }
+        if (this.props.hasOwnProperty('bike')) {
+            this.setState({bike: this.props.bike});
+        } else {
+            this.setState({bike: mockBike});
+        }
+    }
+
+        render () {
+        return (
+            <View style={styles.container}>
+                <View style={styles.viewRow}>
+                    <View style={styles.viewSeparator}>
+                        <Text style={styles.textSeparator} >Bevorstehende Mieten</Text>
+                    </View>
+                    <View style={styles.viewBorder}>
+                        <Text style={styles.textSeparator} >Bevorstehende Mieten</Text>
+                    </View>
+                    <View style={styles.viewSeparator}>
+                        <Text style={styles.textSeparator} >Vergangene Mieten</Text>
+                    </View>
+                    <View style={styles.viewBorder}>
+                        <Text style={styles.textSeparator} >Bevorstehende Mieten</Text>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
---
+++ src/styles/common.js
@@ -0,0 +1,35 @@
+import {
+    StyleSheet,
+    Dimensions,
+} from 'react-native';
+
+export const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
+
+// colors
+export const backgroundColor = '#FFFFFF';           // general app background
+export const primaryColor = '#777777';              // primary dark font color
+export const secondaryColor = '#C8C8C8';            // secondary light font color
+export const primaryInvertedColor = '#FFFFFF';      // contrast color for primary color (text for primary buttons, background for secondary buttons)
+export const inactiveColor = '#CCCCCC';             // inactive button text color
+export const inactiveBackgroundColor = '#F8F8F8';   // inactive button background
+
+export const whiteOpacityColor4 = 'rgba(255, 255, 255, 0.4)';
+
+// font sizes
+export const largeFontSize = 24;
+export const standardFontSize = 18;
+export const smallFontSize = 12;
+
+// borders
+export const standardBorderRadius = 3;
+export const standardBorderWidth = 2;
+export const separatorBorderWidth = 2;
+
+
+// styles
+export default StyleSheet.create({
+    buttonGreenWrapper: {
+        justifyContent: 'center',
+    },
+
+});
